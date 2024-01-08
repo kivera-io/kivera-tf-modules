463473698149e105de0b2f2c1d655f16922d409c
tyler-matheson <tyler.matheson@outlook.com.au>
2024-01-08 16:43:17 +1100

    Kpr 891 (#2)
    * feat: cw logging, split lb subnets, deploy redis cluster
    
    * feat: performance tuning, redis connection fixes
    
    * fix: udpate readme

--- README.md
+++ README.md
@@ -12,9 +12,12 @@ module "kivera" {
   proxy_instance_type         = var.proxy_instance_type
   key_pair_name               = var.key_pair_name
   vpc_id                      = var.vpc_id
-  subnet_ids                  = var.subnet_ids
+  proxy_subnet_ids            = var.proxy_subnet_ids
+  load_balancer_subnet_ids    = var.load_balancer_subnet_ids
   load_balancer_internal      = var.load_balancer_internal
   proxy_allowed_ingress_range = var.proxy_allowed_ingress_range
   proxy_allowed_ssh_range     = var.proxy_allowed_ssh_range
+  redis_cache_enabled         = var.redis_cache_enabled
+  redis_subnet_ids            = var.redis_subnet_ids
 }
 ```
--- proxy/aws-autoscaled-simple-scaling/main.tf
+++ proxy/aws-autoscaled-simple-scaling/main.tf
@@ -8,9 +8,16 @@ data "aws_ami" "latest" {
   }
 }
 
+resource "random_string" "suffix" {
+  length  = 10
+  special = false
+}
+
 locals {
+  suffix = random_string.suffix.result
   proxy_credentials_secret_arn = var.proxy_credentials != "" ? aws_secretsmanager_secret_version.proxy_credentials_version[0].arn : var.proxy_credentials_secret_arn
   proxy_private_key_secret_arn = var.proxy_private_key != "" ? aws_secretsmanager_secret_version.proxy_private_key_version[0].arn : var.proxy_private_key_secret_arn
+  redis_connection_string      = var.redis_cache_enabled ? "rediss://${aws_elasticache_replication_group.redis[0].configuration_endpoint_address}:6379" : ""
 }
 
 resource "aws_secretsmanager_secret" "proxy_credentials" {
@@ -25,12 +32,12 @@ resource "aws_secretsmanager_secret_version" "proxy_credentials_version" {
 }
 
 resource "aws_secretsmanager_secret" "proxy_private_key" {
-  count       = var.proxy_private_key != "" ?  1 : 0
+  count       = var.proxy_private_key != "" ? 1 : 0
   name_prefix = "${var.name_prefix}-private-key-"
 }
 
 resource "aws_secretsmanager_secret_version" "proxy_private_key_version" {
-  count         = var.proxy_private_key != "" ?  1 : 0
+  count         = var.proxy_private_key != "" ? 1 : 0
   secret_id     = aws_secretsmanager_secret.proxy_private_key[0].id
   secret_string = var.proxy_private_key
 }
@@ -164,6 +171,12 @@ resource "aws_launch_template" "launch_template" {
   ]
   key_name  = var.key_pair_name
   user_data = base64encode(data.template_file.proxy_user_data.rendered)
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = 50
+    }
+  }
 }
 
 data "template_file" "proxy_user_data" {
@@ -174,6 +187,9 @@ data "template_file" "proxy_user_data" {
     proxy_public_cert            = var.proxy_public_cert
     proxy_credentials_secret_arn = local.proxy_credentials_secret_arn
     proxy_private_key_secret_arn = local.proxy_private_key_secret_arn
+    redis_connection_string      = local.redis_connection_string
+    log_group_name               = "${var.name_prefix}-proxy-${local.suffix}"
+    log_group_retention_in_days  = var.proxy_log_group_retention
   }
 }
 
@@ -187,7 +203,7 @@ resource "aws_autoscaling_group" "auto_scaling_group" {
   max_size                  = var.proxy_max_asg_size
   health_check_type         = "ELB"
   health_check_grace_period = 180
-  vpc_zone_identifier       = var.subnet_ids
+  vpc_zone_identifier       = var.proxy_subnet_ids
   target_group_arns = [
     aws_lb_target_group.traffic_target_group.arn,
     aws_lb_target_group.management_target_group.arn
@@ -196,7 +212,7 @@ resource "aws_autoscaling_group" "auto_scaling_group" {
     strategy = "Rolling"
     triggers = ["tag"]
     preferences {
-      auto_rollback = true
+      auto_rollback          = true
       min_healthy_percentage = 100
     }
   }
@@ -208,7 +224,7 @@ resource "aws_autoscaling_group" "auto_scaling_group" {
 }
 
 resource "aws_lb_target_group" "traffic_target_group" {
-  name = "${var.name_prefix}-traffic"
+  name = "${var.name_prefix}-traffic-${local.suffix}"
   health_check {
     enabled             = true
     interval            = 10
@@ -234,7 +250,7 @@ resource "aws_lb_listener" "traffic_listener" {
 }
 
 resource "aws_lb_target_group" "management_target_group" {
-  name = "${var.name_prefix}-mgmt"
+  name = "${var.name_prefix}-mgmt-${local.suffix}"
   health_check {
     enabled             = true
     interval            = 10
@@ -263,9 +279,9 @@ resource "aws_lb_listener" "management_listener" {
 }
 
 resource "aws_lb" "load_balancer" {
-  name               = "${var.name_prefix}-load-balancer"
+  name               = "${var.name_prefix}-load-balancer-${local.suffix}"
   internal           = var.load_balancer_internal
-  subnets            = var.subnet_ids
+  subnets            = var.load_balancer_subnet_ids
   load_balancer_type = "network"
   security_groups = [
     aws_security_group.load_balancer_sg.id
@@ -277,7 +293,7 @@ resource "aws_autoscaling_policy" "scale_up_policy" {
   name                   = "${var.name_prefix}-scale-up"
   scaling_adjustment     = 3
   adjustment_type        = "ChangeInCapacity"
-  cooldown               = 120
+  cooldown               = 180
   autoscaling_group_name = aws_autoscaling_group.auto_scaling_group.name
 }
 
@@ -323,4 +339,61 @@ resource "aws_cloudwatch_metric_alarm" "cpu_alarm_low" {
     AutoScalingGroupName = aws_autoscaling_group.auto_scaling_group.name
   }
   comparison_operator = "LessThanThreshold"
+}
+
+resource "aws_security_group" "redis_sg" {
+  count = var.redis_cache_enabled ? 1 : 0
+
+  name_prefix = "${var.name_prefix}-redis-"
+  vpc_id      = var.vpc_id
+  description = "Access to the Redis cache"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "redis_ingress_rule" {
+  count = var.redis_cache_enabled ? 1 : 0
+
+  security_group_id            = aws_security_group.redis_sg[0].id
+  referenced_security_group_id = aws_security_group.instance_sg.id
+  ip_protocol                  = "tcp"
+  from_port                    = 6379
+  to_port                      = 6379
+}
+
+resource "aws_vpc_security_group_egress_rule" "redis_egress_rule" {
+  count = var.redis_cache_enabled ? 1 : 0
+
+  security_group_id = aws_security_group.redis_sg[0].id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = -1
+}
+
+resource "aws_elasticache_subnet_group" "redis" {
+  count = var.redis_cache_enabled ? 1 : 0
+
+  name       = "${var.name_prefix}-subnet-group-${local.suffix}"
+  subnet_ids = var.redis_subnet_ids
+}
+
+resource "aws_elasticache_replication_group" "redis" {
+  count = var.redis_cache_enabled ? 1 : 0
+
+  replication_group_id = "${var.name_prefix}-redis-${local.suffix}"
+  description          = "Redis Cache for Kivera proxy"
+
+  node_type            = var.redis_instance_type
+  engine_version       = 7.1
+  parameter_group_name = "default.redis7.cluster.on"
+
+  port               = 6379
+  subnet_group_name  = aws_elasticache_subnet_group.redis[0].name
+  security_group_ids = [aws_security_group.redis_sg[0].id]
+
+  multi_az_enabled           = true
+  num_node_groups            = var.redis_num_node_groups
+  replicas_per_node_group    = var.redis_replicas_per_node_group
+  automatic_failover_enabled = true
+  at_rest_encryption_enabled = true
+  transit_encryption_enabled = true
+
+  apply_immediately = true
 }
