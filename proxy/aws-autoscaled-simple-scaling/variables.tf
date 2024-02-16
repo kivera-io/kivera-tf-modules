21fcd603534f327525e005d783800a1177b1da30
Marcus Lean <marcuslean@gmail.com>
2024-02-16 16:07:36 +1100

    feat: generalise cache

--- proxy/aws-autoscaled-simple-scaling/variables.tf
+++ proxy/aws-autoscaled-simple-scaling/variables.tf
@@ -126,19 +126,30 @@ variable "proxy_log_group_retention" {
   default     = 14
 }
 
-variable "redis_cache_enabled" {
+variable "cache_enabled" {
   description = "Whether to deploy and use a Redis cache"
   type        = bool
   default     = true
 }
 
-variable "redis_subnet_ids" {
+variable "cache_type" {
+  description = "What type of cache to deploy"
+  type        = string
+  default     = "redis"
+
+  validation {
+    condition     = contains(["redis"], var.cache_type)
+    error_message = "Allowed value(s) for cache_type: \"redis\"."
+  }
+}
+
+variable "cache_subnet_ids" {
   description = "Which Subnets to deploy the Redis cluster into"
   type        = list(string)
   default     = []
 }
 
-variable "redis_instance_type" {
+variable "cache_instance_type" {
   description = "The ElastiCache Instance Type of the Redis nodes"
   type        = string
   default     = "cache.t3.medium"
