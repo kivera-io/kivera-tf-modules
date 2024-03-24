87df4f51cf53c67f4c49e10e6226b5915dea7920
Marcus Lean <marcuslean@gmail.com>
2024-03-25 10:30:29 +1100

    fix: subnet for vpc endpoint

--- proxy/aws-autoscaled-simple-scaling-transparent/main.tf
+++ proxy/aws-autoscaled-simple-scaling-transparent/main.tf
@@ -252,7 +252,7 @@ resource "aws_vpc_endpoint_service" "vpc_endpoint_service" {
 resource "aws_vpc_endpoint" "glb_endpoint" {
   vpc_id            = var.vpc_id
   vpc_endpoint_type = "GatewayLoadBalancer"
-  subnet_ids        = var.private_subnet_ids
+  subnet_ids        = [var.private_subnet_ids[0]]
   service_name      = aws_vpc_endpoint_service.vpc_endpoint_service.service_name
 
   tags = {
