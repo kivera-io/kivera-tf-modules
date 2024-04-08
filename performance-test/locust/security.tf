a52b7714798b6be598079d735caa54453bbbf310
Marcus Lean <marcuslean@gmail.com>
2024-04-08 16:47:41 +1000

    fix: added missing ssh ingress

--- performance-test/locust/security.tf
+++ performance-test/locust/security.tf
@@ -14,6 +14,14 @@ resource "aws_security_group" "locust" {
   }
 
   ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "HTTP"
     from_port   = 80
     to_port     = 80
