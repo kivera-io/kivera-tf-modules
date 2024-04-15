06b52b0686e9ccb22dfeade888235e62cdd0d38a
Marcus Lean <marcuslean@gmail.com>
2024-04-15 16:31:06 +1000

    temp: s3 tests

--- proxy/aws-autoscaled-simple-scaling-transparent/data/tunnel-handler.sh
+++ proxy/aws-autoscaled-simple-scaling-transparent/data/tunnel-handler.sh
@@ -13,10 +13,32 @@ INSTANCE_ID=$(curl 169.254.169.254/latest/meta-data/instance-id)
 case $1 in
         CREATE)
 
-                echo "==> Setting up simple passthrough"
-                echo Mode is $1, In Int is $2, Out Int is $3, ENI is $4
-                tc qdisc add dev $2 ingress
-                tc filter add dev $2 parent ffff: protocol all prio 2 u32 match u32 0 0 flowid 1:1 action mirred egress mirror dev $3
+                iptables -P INPUT ACCEPT
+                iptables -P FORWARD ACCEPT
+                iptables -P OUTPUT ACCEPT
+
+                iptables -t nat -F
+                iptables -t mangle -F
+                iptables -F
+                iptables -X
+
+                echo "Removing IP FORWARD"
+                echo 0 > /proc/sys/net/ipv4/ip_forward
+                echo 1 > /proc/sys/net/ipv4/conf/all/rp_filter
+                echo 1 > /proc/sys/net/ipv4/conf/$2/rp_filter
+
+                curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document > /home/ec2-user/iid;
+                export instance_id=$(cat /home/ec2-user/iid | jq -r .instanceId);
+                export instance_ip=$(cat /home/ec2-user/iid | jq -r .privateIp);
+                echo "Setting up NAT and IP FORWARD"
+                # iptables -t nat -A PREROUTING -p tcp -m tcp ! -d $instance_ip --dport 443 -j DNAT --to-destination $instance_ip:10080
+                # iptables -t nat -A PREROUTING -p tcp -m tcp ! -d $instance_ip --dport 443 -j REDIRECT --to-ports 10080
+                iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+                iptables -A FORWARD -i $2 -o $2 -j ACCEPT
+
+                echo 1 > /proc/sys/net/ipv4/ip_forward
+                echo 0 > /proc/sys/net/ipv4/conf/all/rp_filter
+                echo 0 > /proc/sys/net/ipv4/conf/$2/rp_filter
                 ;;
 
         DESTROY)
